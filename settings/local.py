4a8f7d3c31e6c74a8d91ba277f97b1b7a3f69f80
fizteh95 <causeLove95@yandex.ru>
2024-05-01 17:04:07 +0500

    docker

--- settings/local.py
+++ settings/local.py
@@ -1,8 +1,8 @@
 from os import getenv
 
 # TG_TOKEN = getenv("TG_TOKEN", "6760495857:AAFuuJ0PYCJISPF75bPJpefy_-eLOkI8ptk")
-DB_HOST = getenv("TG_DB_HOST", "localhost")
-DB_PORT = getenv("TG_DB_PORT", "5432")
-DB_NAME = getenv("TG_DB_NAME", "postgres")
-DB_USER = getenv("TG_DB_USER", "postgres")
-DB_PASSWORD = getenv("TG_DB_PASSWORD", "postgrespostgres")
+DB_HOST = getenv("DB_HOST", "localhost")
+DB_PORT = getenv("DB_PORT", "5432")
+DB_NAME = getenv("DB_NAME", "postgres")
+DB_USER = getenv("DB_USER", "postgres")
+DB_PASSWORD = getenv("DB_PASSWORD", "postgrespostgres")
